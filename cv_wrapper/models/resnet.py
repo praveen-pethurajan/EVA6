982be9a9ac37106d2e280c270ae524ad5fd4bf44
Praveen Pethurajan <75525971+praveen-pethurajan@users.noreply.github.com>
2021-07-03 13:39:48 +0530

    update resnet with group normalization

--- cv_wrapper/models/resnet.py
+++ cv_wrapper/models/resnet.py
@@ -16,17 +16,17 @@ class BasicBlock(nn.Module):
         super(BasicBlock, self).__init__()
         self.conv1 = nn.Conv2d(
             in_channels, channels, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(channels)
+        self.bn1 = nn.GroupNorm(1, channels)
         self.conv2 = nn.Conv2d(channels, channels, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(channels)
+        self.bn2 = nn.GroupNorm(1, channels)
 
         self.shortcut = nn.Sequential()
         if stride != 1 or in_channels != self.expansion*channels:
             self.shortcut = nn.Sequential(
                 nn.Conv2d(in_channels, self.expansion*channels,
                           kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion*channels)
+                nn.GroupNorm(1, self.expansion*channels)
             )
 
     def forward(self, x):
@@ -36,7 +36,6 @@ class BasicBlock(nn.Module):
         out = F.relu(out)
         return out
 
-
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=10):
         super(ResNet, self).__init__()
@@ -44,11 +43,11 @@ class ResNet(nn.Module):
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
                                stride=1, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
+        self.bn1 = nn.GroupNorm(1, 64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
         self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=1)
         self.linear = nn.Linear(512*block.expansion, num_classes)
 
     def _make_layer(self, block, channels, num_blocks, stride):
@@ -65,23 +64,18 @@ class ResNet(nn.Module):
         out = self.layer2(out)
         out = self.layer3(out)
         out = self.layer4(out)
-        out = F.avg_pool2d(out, 4)
+        out = F.avg_pool2d(out, 8)
         out = out.view(out.size(0), -1)
         out = self.linear(out)
         return out
 
-
 def ResNet18():
     return ResNet(BasicBlock, [2, 2, 2, 2])
 
-
 def ResNet34():
     return ResNet(BasicBlock, [3, 4, 6, 3])
-
 
 def test():
     net = ResNet18()
     y = net(torch.randn(1, 3, 32, 32))
     print(y.size())
-
-# test()
